1d3c054f3eeed59db06593038e77bc171d392336
E.Z. Hart <hartez@gmail.com>
2016-07-13 10:04:51 -0600

    Very slightly reduced code

--- Xamarin.Forms.Core/PlatformSpecifics.cs
+++ Xamarin.Forms.Core/PlatformSpecifics.cs
@@ -9,27 +9,16 @@ namespace Xamarin.Forms
 
 	public interface IMasterDetailPagePlatformConfiguration
 	{
-		IMasterDetailPageWindowsConfiguration OnWindows();
-
 		IMasterDetailPageAndroidConfiguration OnAndroid();
-
 		IMasterDetailPageiOSConfiguration OniOS();
+		IMasterDetailPageWindowsConfiguration OnWindows();
 	}
+
+	#region Windows
 
 	public interface IMasterDetailPageWindowsConfiguration : IElementConfiguration<MasterDetailPage>
 	{
 		CollapseStyle CollapseStyle { get; set; }
-	}
-
-	public interface IMasterDetailPageAndroidConfiguration : IElementConfiguration<MasterDetailPage>
-	{
-		int SomeAndroidThing { get; set; }
-
-		int SomeOtherAndroidThing { get; set; }
-	}
-
-	public interface IMasterDetailPageiOSConfiguration
-	{
 	}
 
 	public class MasterDetailPageWindowsConfiguration : IMasterDetailPageWindowsConfiguration
@@ -39,29 +28,51 @@ namespace Xamarin.Forms
 			Element = element;
 		}
 
-		public CollapseStyle CollapseStyle 
+		public MasterDetailPage Element { get; }
+
+		public CollapseStyle CollapseStyle
 		{
 			get { return (CollapseStyle)Element.GetValue(MasterDetailPageWindowsSpecifics.CollapseStyleProperty); }
 			set { Element.SetValue(MasterDetailPageWindowsSpecifics.CollapseStyleProperty, value); }
 		}
-
-		public MasterDetailPage Element { get; }
 	}
 
 	public static class MasterDetailPageWindowsSpecifics
 	{
-		public static readonly BindableProperty CollapseStyleProperty = BindableProperty.Create("CollapseStyle", typeof(CollapseStyle),
+		public static readonly BindableProperty CollapseStyleProperty = BindableProperty.Create("CollapseStyle",
+			typeof(CollapseStyle),
 			typeof(MasterDetailPage), CollapseStyle.None);
-
-		public static void SetCollapseStyle(this MasterDetailPage mdp, CollapseStyle value)
-		{
-			mdp.SetValue(CollapseStyleProperty, value);
-		}
 
 		public static CollapseStyle GetCollapseStyle(this MasterDetailPage mdp)
 		{
 			return (CollapseStyle)mdp.GetValue(CollapseStyleProperty);
 		}
+
+		public static void SetCollapseStyle(this MasterDetailPage mdp, CollapseStyle value)
+		{
+			mdp.SetValue(CollapseStyleProperty, value);
+		}
+	}
+
+	public static class MasterDetailPageWindowsConfigurationExtensions
+	{
+		public static IMasterDetailPageWindowsConfiguration UsePartialCollapse(
+			this IMasterDetailPageWindowsConfiguration config)
+		{
+			config.CollapseStyle = CollapseStyle.Partial;
+			return config;
+		}
+	}
+
+	#endregion
+
+	#region Android
+
+	public interface IMasterDetailPageAndroidConfiguration : IElementConfiguration<MasterDetailPage>
+	{
+		int SomeAndroidThing { get; set; }
+
+		int SomeOtherAndroidThing { get; set; }
 	}
 
 	internal class MasterDetailPageAndroidConfiguration : IMasterDetailPageAndroidConfiguration
@@ -71,7 +82,9 @@ namespace Xamarin.Forms
 			Element = element;
 		}
 
-		public int SomeAndroidThing 
+		public MasterDetailPage Element { get; }
+
+		public int SomeAndroidThing
 		{
 			get { return (int)Element.GetValue(MasterDetailPageAndroidSpecifics.SomeAndroidThingProperty); }
 			set { Element.SetValue(MasterDetailPageAndroidSpecifics.SomeAndroidThingProperty, value); }
@@ -82,25 +95,14 @@ namespace Xamarin.Forms
 			get { return (int)Element.GetValue(MasterDetailPageAndroidSpecifics.SomeOtherAndroidThingProperty); }
 			set { Element.SetValue(MasterDetailPageAndroidSpecifics.SomeOtherAndroidThingProperty, value); }
 		}
-
-		public MasterDetailPage Element { get; }
-	}
-
-	internal class MasterDetailPageiOsConfiguration : IMasterDetailPageiOSConfiguration
-	{
-		public MasterDetailPageiOsConfiguration(MasterDetailPage element)
-		{
-			Element = element;
-		}
-
-		public MasterDetailPage Element { get; }
 	}
 
 	public static class MasterDetailPageAndroidSpecifics
 	{
 		#region Properties
 
-		public static readonly BindableProperty SomeAndroidThingProperty = BindableProperty.Create("SomeAndroidThing", typeof(int),
+		public static readonly BindableProperty SomeAndroidThingProperty = BindableProperty.Create("SomeAndroidThing",
+			typeof(int),
 			typeof(MasterDetailPage), 1);
 
 		public static void SetSomeAndroidThing(this MasterDetailPage mdp, int value)
@@ -113,10 +115,11 @@ namespace Xamarin.Forms
 			return (int)mdp.GetValue(SomeAndroidThingProperty);
 		}
 
-		public static readonly BindableProperty SomeOtherAndroidThingProperty = BindableProperty.Create("SomeOtherAndroidThing", typeof(int),
-			typeof(MasterDetailPage), 1);
+		public static readonly BindableProperty SomeOtherAndroidThingProperty =
+			BindableProperty.Create("SomeOtherAndroidThing", typeof(int),
+				typeof(MasterDetailPage), 1);
 
-		public static void SetSomeOtherAndroidThingThing(this MasterDetailPage mdp, int value)
+		public static void SetSomeOtherAndroidThing(this MasterDetailPage mdp, int value)
 		{
 			mdp.SetValue(SomeOtherAndroidThingProperty, value);
 		}
@@ -126,12 +129,12 @@ namespace Xamarin.Forms
 			return (int)mdp.GetValue(SomeOtherAndroidThingProperty);
 		}
 
-
 		#endregion
 
 		#region Configuration
 
-		public static IMasterDetailPageAndroidConfiguration UseTabletDefaults(this IMasterDetailPageAndroidConfiguration config)
+		public static IMasterDetailPageAndroidConfiguration UseTabletDefaults(
+			this IMasterDetailPageAndroidConfiguration config)
 		{
 			config.SomeAndroidThing = 10;
 			config.SomeOtherAndroidThing = 45;
@@ -139,7 +142,8 @@ namespace Xamarin.Forms
 			return config;
 		}
 
-		public static IMasterDetailPageAndroidConfiguration UsePhabletDefaults(this IMasterDetailPageAndroidConfiguration config)
+		public static IMasterDetailPageAndroidConfiguration UsePhabletDefaults(
+			this IMasterDetailPageAndroidConfiguration config)
 		{
 			config.SomeAndroidThing = 8;
 			config.SomeOtherAndroidThing = 40;
@@ -155,7 +159,8 @@ namespace Xamarin.Forms
 			return config;
 		}
 
-		public static IMasterDetailPageAndroidConfiguration SetThing(this IMasterDetailPageAndroidConfiguration config, int value)
+		public static IMasterDetailPageAndroidConfiguration SetThing(this IMasterDetailPageAndroidConfiguration config,
+			int value)
 		{
 			config.SomeAndroidThing = value;
 			return config;
@@ -164,92 +169,140 @@ namespace Xamarin.Forms
 		#endregion
 	}
 
-	public static class MasterDetailPageWindowsConfigurationExtensions
+	#endregion
+
+	#region iOS
+
+	public interface IMasterDetailPageiOSConfiguration : IElementConfiguration<MasterDetailPage>
 	{
-		public static IMasterDetailPageWindowsConfiguration UsePartialCollapse(this IMasterDetailPageWindowsConfiguration config)
+	}
+
+	internal class MasterDetailPageiOsConfiguration : EmptyElementConfiguration<MasterDetailPage>,
+		IMasterDetailPageiOSConfiguration
+	{
+		public MasterDetailPageiOsConfiguration(MasterDetailPage element) : base(element)
 		{
-			config.CollapseStyle = CollapseStyle.Partial;
-			return config;
 		}
 	}
 
 	#endregion
 
-	#region Translucent
+	#endregion
+
+	#region NavigationPage 
 
 	public interface INavigationPagePlatformConfiguration
 	{
-		INavigationPageWindowsConfiguration OnWindows();
-
 		INavigationPageAndroidConfiguration OnAndroid();
-
 		INavigationPageiOSConfiguration OniOS();
+		INavigationPageWindowsConfiguration OnWindows();
 	}
+
+	#region Windows
+
+	public interface INavigationPageWindowsConfiguration : IElementConfiguration<NavigationPage>
+	{
+	}
+
+	public class NavigationPageWindowsConfiguration : EmptyElementConfiguration<NavigationPage>,
+		INavigationPageWindowsConfiguration
+	{
+		public NavigationPageWindowsConfiguration(NavigationPage element) : base(element)
+		{
+		}
+	}
+
+	#endregion
+
+	#region Android
+
+	public interface INavigationPageAndroidConfiguration : IElementConfiguration<NavigationPage>
+	{
+	}
+
+	public class NavigationPageAndroidConfiguration : EmptyElementConfiguration<NavigationPage>,
+		INavigationPageAndroidConfiguration
+	{
+		public NavigationPageAndroidConfiguration(NavigationPage element) : base(element)
+		{
+		}
+	}
+
+	#endregion
+
+	#region iOS
 
 	public interface INavigationPageiOSConfiguration : IElementConfiguration<NavigationPage>
 	{
 		bool IsNavigationBarTranslucent { get; set; }
 	}
 
-	public interface INavigationPageAndroidConfiguration : IElementConfiguration<NavigationPage>
-	{
-	}
-
-	public interface INavigationPageWindowsConfiguration : IElementConfiguration<NavigationPage>
-	{
-	}
-
 	public class NavigationPageiOSConfiguration : INavigationPageiOSConfiguration
 	{
-		public NavigationPageiOSConfiguration (NavigationPage element)
+		public NavigationPageiOSConfiguration(NavigationPage element)
 		{
 			Element = element;
 		}
 
 		public NavigationPage Element { get; }
 
-		public bool IsNavigationBarTranslucent 
+		public bool IsNavigationBarTranslucent
 		{
 			get { return (bool)Element.GetValue(NavigationPageiOSpecifics.IsNavigationBarTranslucentProperty); }
 			set { Element.SetValue(NavigationPageiOSpecifics.IsNavigationBarTranslucentProperty, value); }
 		}
 	}
 
-	public class NavigationPageAndroidConfiguration : INavigationPageAndroidConfiguration
-	{
-		public NavigationPageAndroidConfiguration (NavigationPage element)
-		{
-			Element = element;
-		}
-
-		public NavigationPage Element { get; }
-	}
-
-
-	public class NavigationPageWindowsConfiguration : INavigationPageWindowsConfiguration
-	{
-		public NavigationPageWindowsConfiguration(NavigationPage element)
-		{
-			Element = element;
-		}
-
-		public NavigationPage Element { get; }
-	}
-
-
 	public static class NavigationPageiOSpecifics
 	{
-		public static readonly BindableProperty IsNavigationBarTranslucentProperty = BindableProperty.Create("IsNavigationBarTranslucent", typeof(bool),
-			typeof(NavigationPage), false);
+		public static readonly BindableProperty IsNavigationBarTranslucentProperty =
+			BindableProperty.Create("IsNavigationBarTranslucent", typeof(bool),
+				typeof(NavigationPage), false);
+
+		public static bool GetIsNavigationBarTranslucent(this NavigationPage navigationPage)
+		{
+			return (bool)navigationPage.GetValue(IsNavigationBarTranslucentProperty);
+		}
 
 		public static void SetIsNavigationBarTranslucent(this NavigationPage navigationPage, bool value)
 		{
 			navigationPage.SetValue(IsNavigationBarTranslucentProperty, value);
 		}
+	}
 
-		public static bool GetIsNavigationBarTranslucent(this NavigationPage navigationPage)
+	#endregion
+
+	#endregion
+
+	public class EmptyElementConfiguration<T> : IElementConfiguration<T> where T : Element
+	{
+		public EmptyElementConfiguration(T element)
 		{
-			return (bool)navigationPage.GetValue(IsNavigationBarTranslucentProperty);
+			Element = element;
+		}
+
+		public T Element { get; }
+	}
+
+	#region Vendor
+	
+	public static class FakeVendorExtensions
+	{
+		public static readonly BindableProperty FooProperty = BindableProperty.Create("Foo", typeof(bool), typeof(MasterDetailPage), true);
+
+		public static void SetFoo(this MasterDetailPage mdp, bool value)
+		{
+			mdp.SetValue(FooProperty, value);
+		}
+
+		public static bool GetFoo(this MasterDetailPage mdp)
+		{
+			return (bool)mdp.GetValue(FooProperty);
+		}
+
+		public static void SetVendorFoo(this IMasterDetailPageWindowsConfiguration config, bool value)
+		{
+			config.Element.SetFoo(value);
 		}
 	}
 
