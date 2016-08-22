995ef932805afb3116f2a4bfbeaa9d7508ed859c
Rui Marinho <me@ruimarinho.net>
2016-08-22 15:36:28 +0100

    [MacOS] Add SwitchRenderer

--- Xamarin.Forms.Platform.MacOS/Properties/AssemblyInfo.cs
+++ Xamarin.Forms.Platform.MacOS/Properties/AssemblyInfo.cs
@@ -52,3 +52,4 @@ using Xamarin.Forms.Internals;
 [assembly: ExportRenderer(typeof(SearchBar), typeof(SearchBarRenderer))]
 [assembly: ExportRenderer(typeof(Slider), typeof(SliderRenderer))]
 [assembly: ExportRenderer(typeof(Stepper), typeof(StepperRenderer))]
+[assembly: ExportRenderer(typeof(Switch), typeof(SwitchRenderer))]
