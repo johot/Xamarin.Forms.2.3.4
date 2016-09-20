63b05d750cd3ad319367da228d31546c8b65d3af
Rui Marinho <me@ruimarinho.net>
2016-09-20 16:14:10 +0100

    [MacOS] Add GestureRecognizers

--- Xamarin.Forms.Platform.MacOS/EventTracker.cs
+++ Xamarin.Forms.Platform.MacOS/EventTracker.cs
@@ -18,7 +18,7 @@ namespace Xamarin.Forms.Platform.MacOS
 		NSView _handler;
 
 		double _previousScale = 1.0;
-		//UITouchEventArgs _shouldReceive;
+		NSGestureEvent _shouldReceive;
 
 		public EventTracker(IVisualElementRenderer renderer)
 		{
@@ -67,7 +67,7 @@ namespace Xamarin.Forms.Platform.MacOS
 			if (_disposed)
 				throw new ObjectDisposedException(null);
 
-			//	_shouldReceive = (r, t) => t.View is IVisualElementRenderer;
+			_shouldReceive = (r, t) => r.View is IVisualElementRenderer;
 
 			_handler = handler;
 			OnElementChanged(this, new VisualElementChangedEventArgs(null, _renderer.Element));
@@ -84,147 +84,124 @@ namespace Xamarin.Forms.Platform.MacOS
 			var tapRecognizer = recognizer as TapGestureRecognizer;
 			if (tapRecognizer != null)
 			{
-				//var uiRecognizer = CreateTapRecognizer(1, tapRecognizer.NumberOfTapsRequired, r =>
-				//{
-				//	var tapGestureRecognizer = weakRecognizer.Target as TapGestureRecognizer;
-				//	var eventTracker = weakEventTracker.Target as EventTracker;
-				//	var view = eventTracker?._renderer?.Element as View;
+				var uiRecognizer = CreateTapRecognizer(tapRecognizer.NumberOfTapsRequired, r =>
+				{
+					var tapGestureRecognizer = weakRecognizer.Target as TapGestureRecognizer;
+					var eventTracker = weakEventTracker.Target as EventTracker;
+					var view = eventTracker?._renderer?.Element as View;
 
-				//	if (tapGestureRecognizer != null && view != null)
-				//		tapGestureRecognizer.SendTapped(view);
-				//});
-				//return uiRecognizer;
+					if (tapGestureRecognizer != null && view != null)
+						tapGestureRecognizer.SendTapped(view);
+				});
+				return uiRecognizer;
 			}
 
 			var pinchRecognizer = recognizer as PinchGestureRecognizer;
 			if (pinchRecognizer != null)
 			{
-				//double startingScale = 1;
-				//var uiRecognizer = CreatePinchRecognizer(r =>
-				//{
-				//	var pinchGestureRecognizer = weakRecognizer.Target as IPinchGestureController;
-				//	var eventTracker = weakEventTracker.Target as EventTracker;
-				//	var view = eventTracker?._renderer?.Element as View;
+				double startingScale = 1;
+				var uiRecognizer = CreatePinchRecognizer(r =>
+				{
+					var pinchGestureRecognizer = weakRecognizer.Target as IPinchGestureController;
+					var eventTracker = weakEventTracker.Target as EventTracker;
+					var view = eventTracker?._renderer?.Element as View;
 
-				//	if (pinchGestureRecognizer != null && eventTracker != null && view != null)
-				//	{
-				//		var oldScale = eventTracker._previousScale;
-				//		var originPoint = r.LocationInView(null);
-				//		originPoint = UIApplication.SharedApplication.KeyWindow.ConvertPointToView(originPoint, eventTracker._renderer.NativeView);
-				//		var scaledPoint = new Point(originPoint.X / view.Width, originPoint.Y / view.Height);
+					if (pinchGestureRecognizer != null && eventTracker != null && view != null)
+					{
+						var oldScale = eventTracker._previousScale;
+						var originPoint = r.LocationInView(r.View);
+						originPoint = NSApplication.SharedApplication.KeyWindow.ContentView.ConvertPointToView(originPoint, eventTracker._renderer.NativeView);
+						var scaledPoint = new Point(originPoint.X / view.Width, originPoint.Y / view.Height);
 
-				//		switch (r.State)
-				//		{
-				//			case UIGestureRecognizerState.Began:
-				//				if (r.NumberOfTouches < 2)
-				//					return;
-				//				pinchGestureRecognizer.SendPinchStarted(view, scaledPoint);
-				//				startingScale = view.Scale;
-				//				break;
-				//			case UIGestureRecognizerState.Changed:
-				//				if (r.NumberOfTouches < 2 && pinchGestureRecognizer.IsPinching)
-				//				{
-				//					r.State = UIGestureRecognizerState.Ended;
-				//					pinchGestureRecognizer.SendPinchEnded(view);
-				//					return;
-				//				}
+						switch (r.State)
+						{
+							case NSGestureRecognizerState.Began:
+								pinchGestureRecognizer.SendPinchStarted(view, scaledPoint);
+								startingScale = view.Scale;
+								break;
+							case NSGestureRecognizerState.Changed:
+								var delta = 1.0;
+								var dif = Math.Abs(r.Magnification - oldScale) * startingScale;
+								if (oldScale < r.Magnification)
+									delta = 1 + dif;
+								if (oldScale > r.Magnification)
+									delta = 1 - dif;
 
-				//				var delta = 1.0;
-				//				var dif = Math.Abs(r.Scale - oldScale) * startingScale;
-				//				if (oldScale < r.Scale)
-				//					delta = 1 + dif;
-				//				if (oldScale > r.Scale)
-				//					delta = 1 - dif;
-
-				//				pinchGestureRecognizer.SendPinch(view, delta, scaledPoint);
-				//				eventTracker._previousScale = r.Scale;
-				//				break;
-				//			case UIGestureRecognizerState.Cancelled:
-				//			case UIGestureRecognizerState.Failed:
-				//				if (pinchGestureRecognizer.IsPinching)
-				//					pinchGestureRecognizer.SendPinchCanceled(view);
-				//				break;
-				//			case UIGestureRecognizerState.Ended:
-				//				if (pinchGestureRecognizer.IsPinching)
-				//					pinchGestureRecognizer.SendPinchEnded(view);
-				//				eventTracker._previousScale = 1;
-				//				break;
-				//		}
-				//	}
-				//});
-				//return uiRecognizer;
+								pinchGestureRecognizer.SendPinch(view, delta, scaledPoint);
+								eventTracker._previousScale = r.Magnification;
+								break;
+							case NSGestureRecognizerState.Cancelled:
+							case NSGestureRecognizerState.Failed:
+								if (pinchGestureRecognizer.IsPinching)
+									pinchGestureRecognizer.SendPinchCanceled(view);
+								break;
+							case NSGestureRecognizerState.Ended:
+								if (pinchGestureRecognizer.IsPinching)
+									pinchGestureRecognizer.SendPinchEnded(view);
+								eventTracker._previousScale = 1;
+								break;
+						}
+					}
+				});
+				return uiRecognizer;
 			}
 
 			var panRecognizer = recognizer as PanGestureRecognizer;
 			if (panRecognizer != null)
 			{
-				//var uiRecognizer = CreatePanRecognizer(panRecognizer.TouchPoints, r =>
-				//{
-				//	var eventTracker = weakEventTracker.Target as EventTracker;
-				//	var view = eventTracker?._renderer?.Element as View;
+				var uiRecognizer = CreatePanRecognizer(r =>
+				{
+					var eventTracker = weakEventTracker.Target as EventTracker;
+					var view = eventTracker?._renderer?.Element as View;
 
-				//	var panGestureRecognizer = weakRecognizer.Target as IPanGestureController;
-				//	if (panGestureRecognizer != null && view != null)
-				//	{
-				//		switch (r.State)
-				//		{
-				//			case UIGestureRecognizerState.Began:
-				//				if (r.NumberOfTouches != panRecognizer.TouchPoints)
-				//					return;
-				//				panGestureRecognizer.SendPanStarted(view, Application.Current.PanGestureId);
-				//				break;
-				//			case UIGestureRecognizerState.Changed:
-				//				if (r.NumberOfTouches != panRecognizer.TouchPoints)
-				//				{
-				//					r.State = UIGestureRecognizerState.Ended;
-				//					panGestureRecognizer.SendPanCompleted(view, Application.Current.PanGestureId);
-				//					Application.Current.PanGestureId++;
-				//					return;
-				//				}
-				//				var translationInView = r.TranslationInView(_handler);
-				//				panGestureRecognizer.SendPan(view, translationInView.X, translationInView.Y, Application.Current.PanGestureId);
-				//				break;
-				//			case UIGestureRecognizerState.Cancelled:
-				//			case UIGestureRecognizerState.Failed:
-				//				panGestureRecognizer.SendPanCanceled(view, Application.Current.PanGestureId);
-				//				Application.Current.PanGestureId++;
-				//				break;
-				//			case UIGestureRecognizerState.Ended:
-				//				if (r.NumberOfTouches != panRecognizer.TouchPoints)
-				//				{
-				//					panGestureRecognizer.SendPanCompleted(view, Application.Current.PanGestureId);
-				//					Application.Current.PanGestureId++;
-				//				}
-				//				break;
-				//		}
-				//	}
-				//});
-				//return uiRecognizer;
+					var panGestureRecognizer = weakRecognizer.Target as IPanGestureController;
+					if (panGestureRecognizer != null && view != null)
+					{
+						switch (r.State)
+						{
+							case NSGestureRecognizerState.Began:
+								panGestureRecognizer.SendPanStarted(view, Application.Current.PanGestureId);
+								break;
+							case NSGestureRecognizerState.Changed:
+								var translationInView = r.TranslationInView(_handler);
+								panGestureRecognizer.SendPan(view, translationInView.X, translationInView.Y, Application.Current.PanGestureId);
+								break;
+							case NSGestureRecognizerState.Cancelled:
+							case NSGestureRecognizerState.Failed:
+								panGestureRecognizer.SendPanCanceled(view, Application.Current.PanGestureId);
+								Application.Current.PanGestureId++;
+								break;
+							case NSGestureRecognizerState.Ended:
+								panGestureRecognizer.SendPanCompleted(view, Application.Current.PanGestureId);
+								Application.Current.PanGestureId++;
+								break;
+						}
+					}
+				});
+				return uiRecognizer;
 			}
 
 			return null;
 		}
 
-		//UIPanGestureRecognizer CreatePanRecognizer(int numTouches, Action<UIPanGestureRecognizer> action)
-		//{
-		//	var result = new UIPanGestureRecognizer(action);
-		//	result.MinimumNumberOfTouches = result.MaximumNumberOfTouches = (uint)numTouches;
-		//	return result;
-		//}
+		NSPanGestureRecognizer CreatePanRecognizer(Action<NSPanGestureRecognizer> action)
+		{
+			var result = new NSPanGestureRecognizer(action);
+			return result;
+		}
 
-		//UIPinchGestureRecognizer CreatePinchRecognizer(Action<UIPinchGestureRecognizer> action)
-		//{
-		//	var result = new UIPinchGestureRecognizer(action);
-		//	return result;
-		//}
+		NSMagnificationGestureRecognizer CreatePinchRecognizer(Action<NSMagnificationGestureRecognizer> action)
+		{
+			var result = new NSMagnificationGestureRecognizer(action);
+			return result;
+		}
 
-		//NSTapGestureRecognizer CreateTapRecognizer(int numFingers, int numTaps, Action<UITapGestureRecognizer> action)
-		//{
-		//	var result = new UITapGestureRecognizer(action);
-		//	result.NumberOfTouchesRequired = (uint)numFingers;
-		//	result.NumberOfTapsRequired = (uint)numTaps;
-		//	return result;
-		//}
+		NSClickGestureRecognizer CreateTapRecognizer(int numTaps, Action<NSClickGestureRecognizer> action)
+		{
+			var result = new NSClickGestureRecognizer(action);
+			result.NumberOfClicksRequired = numTaps;
+			return result;
+		}
 
 		void LoadRecognizers()
 		{
@@ -239,7 +216,7 @@ namespace Xamarin.Forms.Platform.MacOS
 				var nativeRecognizer = GetNativeRecognizer(recognizer);
 				if (nativeRecognizer != null)
 				{
-					//nativeRecognizer.ShouldReceiveEvent = _shouldReceive;
+					nativeRecognizer.ShouldReceiveEvent = _shouldReceive;
 					_handler.AddGestureRecognizer(nativeRecognizer);
 
 					_gestureRecognizers[recognizer] = nativeRecognizer;
