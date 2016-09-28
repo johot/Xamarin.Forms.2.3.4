8091a10146b198ff10ccddfb50bb2cb1fc8df8f0
Rui Marinho <me@ruimarinho.net>
2016-09-28 16:39:18 +0100

    [MacOS] Fix AssemblyInfo

--- Xamarin.Forms.Platform.MacOS/Properties/AssemblyInfo.cs
+++ Xamarin.Forms.Platform.MacOS/Properties/AssemblyInfo.cs
@@ -4,26 +4,10 @@ using Xamarin.Forms.Platform.MacOS;
 using Xamarin.Forms;
 using Xamarin.Forms.Internals;
 
-// Information about this assembly is defined by the following attributes. 
-// Change them to the values specific to your project.
-
 [assembly: AssemblyTitle("Xamarin.Forms.Platform.MacOS")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("MacOS Backend for Xamarin.Forms")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("rmarinho")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
-[assembly: AssemblyVersion("1.0.*")]
-
-// The following attributes are used to specify the signing key for the assembly, 
-// if desired. See the Mono documentation for more information about signing.
 
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
@@ -60,6 +44,7 @@ using Xamarin.Forms.Internals;
 [assembly: ExportRenderer(typeof(TimePicker), typeof(TimePickerRenderer))]
 [assembly: ExportRenderer(typeof(WebView), typeof(WebViewRenderer))]
 [assembly: ExportRenderer(typeof(ListView), typeof(ListViewRenderer))]
+[assembly: ExportRenderer(typeof(NativeViewWrapper), typeof(NativeViewWrapperRenderer))]
 
 [assembly: ExportCell(typeof(Cell), typeof(CellRenderer))]
 [assembly: ExportCell(typeof(TextCell), typeof(TextCellRenderer))]
