ab166a2b828db18fbd1f1ce1661f9c08890a81d4
Rui Marinho <me@ruimarinho.net>
2016-10-11 19:39:21 +0100

    [Controls] Remove MainMenu from MacOS

--- Xamarin.Forms.ControlGallery.MacOS/Main.cs
+++ Xamarin.Forms.ControlGallery.MacOS/Main.cs
@@ -2,12 +2,13 @@
 
 namespace Xamarin.Forms.ControlGallery.MacOS
 {
-    static class MainClass
-    {
-        static void Main(string[] args)
-        {
-            NSApplication.Init();
-            NSApplication.Main(args);
-        }
-    }
+	static class MainClass
+	{
+		static void Main(string[] args)
+		{
+			NSApplication.Init();
+			NSApplication.SharedApplication.Delegate = new AppDelegate();
+			NSApplication.Main(args);
+		}
+	}
 }
