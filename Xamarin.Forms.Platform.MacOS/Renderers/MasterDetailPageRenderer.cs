71ba2c40692dd613a971249eb1bb0c8f248ae4ca
Rui Marinho <me@ruimarinho.net>
2016-10-19 15:21:09 +0100

    [MacOS] Fix MDP issues with resizing

--- Xamarin.Forms.Platform.MacOS/Renderers/MasterDetailPageRenderer.cs
+++ Xamarin.Forms.Platform.MacOS/Renderers/MasterDetailPageRenderer.cs
@@ -1,23 +1,21 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Linq;
 using AppKit;
+using CoreGraphics;
 
 namespace Xamarin.Forms.Platform.MacOS
 {
 	public class MasterDetailPageRenderer : NSSplitViewController, IVisualElementRenderer, IEffectControlProvider
 	{
-		const double _masterWidthPercentage = 0.2;
 		bool _disposed;
 		EventTracker _events;
 		VisualElementTracker _tracker;
-
 		MasterDetailPage _masterDetailPage;
 
 		IPageController PageController => Element as IPageController;
 
 		IElementController ElementController => Element as IElementController;
-
-		IMasterDetailPageController MasterDetailPageController => MasterDetailPage as IMasterDetailPageController;
 
 		void IEffectControlProvider.RegisterEffect(Effect effect)
 		{
@@ -26,41 +24,33 @@ namespace Xamarin.Forms.Platform.MacOS
 				platformEffect.Container = View;
 		}
 
-		protected MasterDetailPage MasterDetailPage
-		{
-			get { return _masterDetailPage ?? (_masterDetailPage = (MasterDetailPage)Element); }
-		}
+		protected MasterDetailPage MasterDetailPage => _masterDetailPage ?? (_masterDetailPage = (MasterDetailPage)Element);
 
 		protected override void Dispose(bool disposing)
 		{
 			if (!_disposed && disposing)
 			{
+				PageController?.SendDisappearing();
+
 				if (Element != null)
 				{
-					PageController.SendDisappearing();
 					Element.PropertyChanged -= HandlePropertyChanged;
 					Element = null;
 				}
 
-				if (_tracker != null)
-				{
-					_tracker.Dispose();
-					_tracker = null;
-				}
-
-				if (_events != null)
-				{
-					_events.Dispose();
-					_events = null;
-				}
-
-
-				MasterDetailPage.Master.PropertyChanged -= HandleMasterPropertyChanged;
+				_tracker?.Dispose();
+				_tracker = null;
+				_events?.Dispose();
+				_events = null;
 
 				_disposed = true;
 			}
 			base.Dispose(disposing);
 		}
+
+		public NSViewController ViewController => this;
+
+		public NSView NativeView => View;
 
 		public VisualElement Element { get; private set; }
 
@@ -69,11 +59,6 @@ namespace Xamarin.Forms.Platform.MacOS
 		public SizeRequest GetDesiredSize(double widthConstraint, double heightConstraint)
 		{
 			return NativeView.GetSizeRequest(widthConstraint, heightConstraint);
-		}
-
-		public NSView NativeView
-		{
-			get { return View; }
 		}
 
 		public void SetElement(VisualElement element)
@@ -86,25 +71,14 @@ namespace Xamarin.Forms.Platform.MacOS
 			OnElementChanged(new VisualElementChangedEventArgs(oldElement, element));
 
 			EffectUtilities.RegisterEffectControlProvider(this, oldElement, element);
-
 		}
 
 		public void SetElementSize(Size size)
 		{
 			Element.Layout(new Rectangle(Element.X, Element.Y, size.Width, size.Height));
-			var masterWidth = _masterWidthPercentage * size.Width;
-
-			MasterDetailPage.Master.Layout(new Rectangle(0, 0, masterWidth, size.Height));
-			MasterDetailPage.Detail.Layout(new Rectangle(masterWidth, 0, size.Width - masterWidth, size.Height));
-
-			View.AddConstraint(NSLayoutConstraint.Create(SplitViewItems[0].ViewController.View, NSLayoutAttribute.Width, NSLayoutRelation.LessThanOrEqual, 0.1f, (nfloat)MasterDetailPage.Master.Width));
-			View.AddConstraint(NSLayoutConstraint.Create(SplitViewItems[1].ViewController.View, NSLayoutAttribute.Width, NSLayoutRelation.GreaterThanOrEqual, 0.1f, (nfloat)MasterDetailPage.Detail.Width));
-
-		}
-
-		public NSViewController ViewController
-		{
-			get { return this; }
+			var masterWidth = MasterWidthPercentage * size.Width;
+			SplitViewItems[0].MaximumThickness = SplitViewItems[0].MinimumThickness = (nfloat)masterWidth;
+			UpdateChildrenLayout();
 		}
 
 		protected virtual void OnElementChanged(VisualElementChangedEventArgs e)
@@ -115,20 +89,36 @@ namespace Xamarin.Forms.Platform.MacOS
 			if (e.NewElement != null)
 				e.NewElement.PropertyChanged += HandlePropertyChanged;
 
-			var changed = ElementChanged;
-			if (changed != null)
-				changed(this, e);
+			ElementChanged?.Invoke(this, e);
 		}
+
+		protected virtual double MasterWidthPercentage => 0.3;
 
 		public override void ViewWillAppear()
 		{
-
 			UpdateBackground();
 			_tracker = new VisualElementTracker(this);
 			_events = new EventTracker(this);
 			_events.LoadEvents(NativeView);
 
 			base.ViewWillAppear();
+		}
+
+		public override CGRect GetEffectiveRect(NSSplitView splitView, CGRect proposedEffectiveRect, CGRect drawnRect, nint dividerIndex)
+		{
+			return CGRect.Empty;
+		}
+
+		void UpdateChildrenLayout()
+		{
+			if (View.Frame.Width == -1)
+				return;
+			var width = View.Frame.Width;
+			var height = View.Frame.Height;
+			var masterWidth = MasterWidthPercentage * width;
+			var detailWidth = width - masterWidth;
+			MasterDetailPage.Master.Layout(new Rectangle(0, 0, masterWidth, height));
+			MasterDetailPage.Detail.Layout(new Rectangle(0, 0, detailWidth, height));
 		}
 
 		void HandlePropertyChanged(object sender, PropertyChangedEventArgs e)
@@ -140,6 +130,29 @@ namespace Xamarin.Forms.Platform.MacOS
 				UpdateControllers();
 		}
 
+		void UpdateControllers()
+		{
+			if (Platform.GetRenderer(MasterDetailPage.Master) == null)
+				Platform.SetRenderer(MasterDetailPage.Master, Platform.CreateRenderer(MasterDetailPage.Master));
+			if (Platform.GetRenderer(MasterDetailPage.Detail) == null)
+				Platform.SetRenderer(MasterDetailPage.Detail, Platform.CreateRenderer(MasterDetailPage.Detail));
+
+			ClearControllers();
+
+			UpdateChildrenLayout();
+
+			AddSplitViewItem(new NSSplitViewItem { ViewController = Platform.GetRenderer(MasterDetailPage.Master).ViewController });
+			AddSplitViewItem(new NSSplitViewItem { ViewController = Platform.GetRenderer(MasterDetailPage.Detail).ViewController });
+		}
+
+		void ClearControllers()
+		{
+			while (SplitViewItems.Length > 0)
+				RemoveSplitViewItem(SplitViewItems.Last());
+
+		}
+
+		//TODO: Implement Background color on MDP
 		void UpdateBackground()
 		{
 			//if (!string.IsNullOrEmpty(((Page)Element).BackgroundImage))
@@ -148,46 +161,6 @@ namespace Xamarin.Forms.Platform.MacOS
 			//	View.BackgroundColor = UIColor.White;
 			//else
 			//	View.BackgroundColor = Element.BackgroundColor.ToUIColor();
-		}
-
-
-		void UpdateControllers()
-		{
-			MasterDetailPage.Master.PropertyChanged -= HandleMasterPropertyChanged;
-
-			if (Platform.GetRenderer(MasterDetailPage.Master) == null)
-				Platform.SetRenderer(MasterDetailPage.Master, Platform.CreateRenderer(MasterDetailPage.Master));
-			if (Platform.GetRenderer(MasterDetailPage.Detail) == null)
-				Platform.SetRenderer(MasterDetailPage.Detail, Platform.CreateRenderer(MasterDetailPage.Detail));
-
-			ClearControllers();
-
-			var master = Platform.GetRenderer(MasterDetailPage.Master).ViewController;
-			var detail = Platform.GetRenderer(MasterDetailPage.Detail).ViewController;
-
-			AddSplitViewItem(new NSSplitViewItem { ViewController = master, HoldingPriority = 0.2f });
-			AddSplitViewItem(new NSSplitViewItem { ViewController = detail, HoldingPriority = 0.8f });
-
-
-			MasterDetailPage.Master.PropertyChanged += HandleMasterPropertyChanged;
-		}
-
-		public override CoreGraphics.CGRect GetEffectiveRect(NSSplitView splitView, CoreGraphics.CGRect proposedEffectiveRect, CoreGraphics.CGRect drawnRect, nint dividerIndex)
-		{
-			return CoreGraphics.CGRect.Empty;
-		}
-
-		void ClearControllers()
-		{
-			if (SplitViewItems.Length == 0)
-				return;
-			RemoveSplitViewItem(SplitViewItems[1]);
-			RemoveSplitViewItem(SplitViewItems[0]);
-		}
-
-		void HandleMasterPropertyChanged(object sender, PropertyChangedEventArgs e)
-		{
-
 		}
 	}
 }
