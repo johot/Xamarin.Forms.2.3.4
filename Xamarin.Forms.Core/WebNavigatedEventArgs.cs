7f6b55c587221587c6067e0481befc81c5bbbdf9
E.Z. Hart <hartez@gmail.com>
2016-10-24 11:21:19 -0600

    Put fake method in wrong place

--- Xamarin.Forms.Core/WebNavigatedEventArgs.cs
+++ Xamarin.Forms.Core/WebNavigatedEventArgs.cs
@@ -8,10 +8,10 @@ namespace Xamarin.Forms
 		}
 
 		public WebNavigationResult Result { get; private set; }
-	}
-    
-    internal static void Fake()
-    {
+		
+		internal static void Fake()
+		{
             
-    }
+		}
+	}
 }
