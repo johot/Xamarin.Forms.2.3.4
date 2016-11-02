3449f03b9e3dc0475904918b676f26b6cb210d8b
E.Z. Hart <hartez@gmail.com>
2016-11-02 16:35:56 -0600

    Make Issue198 test work with direct navigation

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/TestPages/TestPages.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/TestPages/TestPages.cs
@@ -33,9 +33,6 @@ namespace Xamarin.Forms.Controls
 #elif __IOS__ 
 			app = ConfigureApp.iOS.InstalledApp (AppPaths.BundleId).Debug ()
 				//Uncomment to run from a specific iOS SIM, get the ID from XCode -> Devices
-.DeviceIdentifier("d8e74947b18cedd091a8ee3c23243e874f1aebbb")
-//          .CodesignIdentity("iPhone Developer: E.Z.Hart(SR9R53YEUX)")
-          //.CodesignIdentity("iPhone Developer")
 				.StartApp ();
 #endif
 			if (app == null)
