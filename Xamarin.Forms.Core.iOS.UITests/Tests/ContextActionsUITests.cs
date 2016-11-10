37e11f7c5d3ae0cc3db25d242147c08ced52b9c4
E.Z. Hart <hartez@gmail.com>
2016-11-10 14:41:03 -0700

    Prevent ContextActions tests from stepping on each other

--- Xamarin.Forms.Core.iOS.UITests/Tests/ContextActionsUITests.cs
+++ Xamarin.Forms.Core.iOS.UITests/Tests/ContextActionsUITests.cs
@@ -45,12 +45,12 @@ namespace Xamarin.Forms.Core.UITests
 		public void ContextActionsDelete ()
 		{
 			// mark is an icon on android
-			App.TouchAndHold (q => q.Marked (cell0));
+			App.TouchAndHold (q => q.Marked (cell1));
 			App.WaitForElement (q => q.Marked (delete));
 			App.Screenshot ("I have actions!");
 
 			App.Tap (q => q.Marked (delete));
-			App.WaitForNoElement (q => q.Marked (cell0));
+			App.WaitForNoElement (q => q.Marked (cell1));
 			App.Screenshot ("Deleted cell 0");
 		}
 #endif
