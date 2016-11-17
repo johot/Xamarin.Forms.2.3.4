4b485c19f7a72d58cdf0b6a5e955268b6386f010
Samantha Houts <samantha.houts@xamarin.com>
2016-11-17 15:42:45 -0800

    Test two small labels next to each other in horizontal layout

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla28650.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla28650.cs
@@ -87,6 +87,54 @@ namespace Xamarin.Forms.Controls
 			grid.AddChild(GetStackLayout("Thisis averylong description withwords thatarelong", LineBreakMode.TailTruncation), column, currentRow++);
 			grid.AddChild(GetStackLayout("Thisis averylong description withwords thatarelong", LineBreakMode.WordWrap), column, currentRow++);
 			grid.AddChild(GetStackLayout("Short desc", LineBreakMode.WordWrap), column, currentRow++);
+
+
+			grid.AddChild(new StackLayout
+			{
+				Orientation = StackOrientation.Horizontal,
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				Children = {
+					new Label
+					{
+						HorizontalOptions = LayoutOptions.FillAndExpand,
+						FontSize = 14,
+						Text = "1",
+						LineBreakMode = LineBreakMode.WordWrap,
+						BackgroundColor = Color.Red
+					}, new Label
+					{
+						HorizontalOptions = LayoutOptions.FillAndExpand,
+						FontSize = 14,
+						Text = "2",
+						LineBreakMode = LineBreakMode.WordWrap,
+						BackgroundColor = Color.Blue
+					}
+				},
+			}, column, currentRow++);
+
+			grid.AddChild(new StackLayout
+			{
+				Orientation = StackOrientation.Horizontal,
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				Children = {
+					new Label
+					{
+						HorizontalOptions = LayoutOptions.FillAndExpand,
+						FontSize = 14,
+						Text = "1",
+						LineBreakMode = LineBreakMode.WordWrap,
+						BackgroundColor = Color.Red
+					}, new Label
+					{
+						HorizontalOptions = LayoutOptions.Start,
+						FontSize = 14,
+						Text = "2",
+						LineBreakMode = LineBreakMode.WordWrap,
+						BackgroundColor = Color.Blue
+					}
+				},
+			}, column, currentRow++);
+
 			Content = grid;
 		}
 		private static StackLayout GetStackLayout(string Text, LineBreakMode breakMode)
