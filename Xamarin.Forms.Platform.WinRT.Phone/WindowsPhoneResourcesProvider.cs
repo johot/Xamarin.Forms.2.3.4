69517d025e6b122c5127cf36c838a4ded2a44b3d
E.Z. Hart <hartez@gmail.com>
2016-11-22 10:50:52 -0700

    Use a prototype TextBlock to resolve theme resources on Windows

--- Xamarin.Forms.Platform.WinRT.Phone/WindowsPhoneResourcesProvider.cs
+++ Xamarin.Forms.Platform.WinRT.Phone/WindowsPhoneResourcesProvider.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Windows.UI.Text;
+﻿using Windows.UI.Text;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using WStyle = Windows.UI.Xaml.Style;
 
 namespace Xamarin.Forms.Platform.WinRT
 {
@@ -12,89 +10,50 @@ namespace Xamarin.Forms.Platform.WinRT
 	{
 		public IResourceDictionary GetSystemResources()
 		{
-			var windowsResources = Windows.UI.Xaml.Application.Current.Resources;
-
-			var resources = new ResourceDictionary ();
-			resources[Device.Styles.TitleStyleKey] = GetStyle ("HeaderTextBlockStyle");
-			resources[Device.Styles.SubtitleStyleKey] = GetStyle ("SubheaderTextBlockStyle");
-			resources[Device.Styles.BodyStyleKey] = GetStyle ("BodyTextBlockStyle");
-			resources[Device.Styles.CaptionStyleKey] = GetStyle ("BodyTextBlockStyle");
-			resources[Device.Styles.ListItemTextStyleKey] = GetStyle ("ListViewItemTextBlockStyle");
-			resources[Device.Styles.ListItemDetailTextStyleKey] = GetStyle ("ListViewItemContentTextBlockStyle");
-			return resources;
+			return new ResourceDictionary
+			{
+				[Device.Styles.TitleStyleKey] = GetStyle("HeaderTextBlockStyle"),
+				[Device.Styles.SubtitleStyleKey] = GetStyle("SubheaderTextBlockStyle"),
+				[Device.Styles.BodyStyleKey] = GetStyle("BodyTextBlockStyle"),
+				[Device.Styles.CaptionStyleKey] = GetStyle("BodyTextBlockStyle"),
+				[Device.Styles.ListItemTextStyleKey] = GetStyle("ListViewItemTextBlockStyle"),
+				[Device.Styles.ListItemDetailTextStyleKey] = GetStyle("ListViewItemContentTextBlockStyle")
+			};
 		}
 
-		Style GetStyle (object nativeKey)
+		Style GetStyle(object nativeKey)
 		{
-			var style = (Windows.UI.Xaml.Style) Windows.UI.Xaml.Application.Current.Resources[nativeKey];
-			style = GetAncestorSetters(style);
+			var style = (WStyle)Windows.UI.Xaml.Application.Current.Resources[nativeKey];
 
-			var formsStyle = new Style (typeof (Label));
-			foreach (var b in style.Setters) {
-				var setter = b as Windows.UI.Xaml.Setter;
-				if (setter == null)
-					continue;
+			_prototype.Style = style;
 
-				// TODO: Need to implement a stealth pass-through for things we don't support
+			var formsStyle = new Style(typeof(Label));
 
-				if (setter.Property == TextBlock.FontSizeProperty)
-					formsStyle.Setters.Add (Label.FontSizeProperty, setter.Value);
-				else if (setter.Property == TextBlock.FontFamilyProperty)
-					formsStyle.Setters.Add (Label.FontFamilyProperty, setter.Value);
-				else if (setter.Property == TextBlock.FontWeightProperty)
-					formsStyle.Setters.Add (Label.FontAttributesProperty, ToAttributes (Convert.ToUInt16 (setter.Value)));
-				else if (setter.Property == TextBlock.TextWrappingProperty)
-					formsStyle.Setters.Add (Label.LineBreakModeProperty, ToLineBreakMode ((TextWrapping) setter.Value));
-			}
+			formsStyle.Setters.Add(Label.FontSizeProperty, _prototype.FontSize);
+			formsStyle.Setters.Add(Label.FontFamilyProperty, _prototype.FontFamily.Source);
+			formsStyle.Setters.Add(Label.FontAttributesProperty, ToAttributes(_prototype.FontWeight));
+			formsStyle.Setters.Add(Label.LineBreakModeProperty, ToLineBreakMode(_prototype.TextWrapping));
 
 			return formsStyle;
 		}
 
-		static Windows.UI.Xaml.Style GetAncestorSetters (Windows.UI.Xaml.Style value)
+		readonly TextBlock _prototype = new TextBlock();
+
+		static FontAttributes ToAttributes(FontWeight fontWeight)
 		{
-			var style = new Windows.UI.Xaml.Style (value.TargetType)
+			if (fontWeight.Weight == FontWeights.Bold.Weight || fontWeight.Weight == FontWeights.SemiBold.Weight 
+				|| fontWeight.Weight == FontWeights.ExtraBold.Weight)
 			{
-				BasedOn = value.BasedOn
-			};
-			foreach (var valSetter in value.Setters)
-			{
-				style.Setters.Add(valSetter);
+				return FontAttributes.Bold;
 			}
 
-			var ancestorStyles = new List<Windows.UI.Xaml.Style> ();
-
-			Windows.UI.Xaml.Style currStyle = style;
-			while (currStyle.BasedOn != null)
-			{
-				ancestorStyles.Add(currStyle.BasedOn);
-				currStyle = currStyle.BasedOn;
-			}
-
-			foreach (var styleParent in ancestorStyles)
-			{
-				foreach (var b in styleParent.Setters)
-				{
-					var parentSetter = b as Windows.UI.Xaml.Setter;
-					if (parentSetter == null)
-						continue;
-
-					var derviedSetter = style.Setters
-						.OfType<Windows.UI.Xaml.Setter> ()
-						.FirstOrDefault (x => x.Property == parentSetter.Property);
-
-					//Ignore any ancestor setters which a child setter has already overridden
-					if (derviedSetter != null)
-						continue;
-					else
-						style.Setters.Add (parentSetter);
-				}
-			}
-			return style;
+			return FontAttributes.None;
 		}
 
-		static LineBreakMode ToLineBreakMode (TextWrapping value)
+		static LineBreakMode ToLineBreakMode(TextWrapping value)
 		{
-			switch (value) {
+			switch (value)
+			{
 				case TextWrapping.Wrap:
 					return LineBreakMode.CharacterWrap;
 				case TextWrapping.WrapWholeWords:
@@ -103,15 +62,6 @@ namespace Xamarin.Forms.Platform.WinRT
 				case TextWrapping.NoWrap:
 					return LineBreakMode.NoWrap;
 			}
-		}
-
-		static FontAttributes ToAttributes (ushort uweight)
-		{
-			if (uweight == FontWeights.Bold.Weight || uweight == FontWeights.SemiBold.Weight || uweight == FontWeights.ExtraBold.Weight) {
-				return FontAttributes.Bold;
-			}
-
-			return FontAttributes.None;
 		}
 	}
 }
