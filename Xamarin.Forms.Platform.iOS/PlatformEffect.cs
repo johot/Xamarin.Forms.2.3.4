e0bf711e1aac723b532efaada15e2b3970049110
Rui Marinho <me@ruimarinho.net>
2016-12-19 19:59:51 +0000

    [MacOS] Share platform effect

--- Xamarin.Forms.Platform.iOS/PlatformEffect.cs
+++ Xamarin.Forms.Platform.iOS/PlatformEffect.cs
@@ -1,6 +1,10 @@
-﻿using UIKit;
-
+﻿#if __MOBILE__
+using UIKit;
 namespace Xamarin.Forms.Platform.iOS
+#else
+using UIView = AppKit.NSView;
+namespace Xamarin.Forms.Platform.MacOS
+#endif
 {
 	public abstract class PlatformEffect : PlatformEffect<UIView, UIView>
 	{
