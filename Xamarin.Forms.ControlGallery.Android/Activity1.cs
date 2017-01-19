ba836d54d08d441ec41cb8a46453677965f04146
Rui Marinho <me@ruimarinho.net>
2017-01-19 22:41:25 +0000

    fix build

--- Xamarin.Forms.ControlGallery.Android/Activity1.cs
+++ Xamarin.Forms.ControlGallery.Android/Activity1.cs
@@ -286,192 +286,190 @@ namespace Xamarin.Forms.ControlGallery.Android
 		HardwareAccelerated = true,
 		ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	[IntentFilter(new[] { Intent.ActionView },
-		Categories = new[]
-                           
-			Intent.ActionView,
-		Intent.CategoryDefault,
-		Intent.CategoryBrowsable
-
-}
-,
-DataScheme = "http",
-DataHost = App.AppName,
-DataPathPrefix = "/gallery/"
-)
-]
-
-public class Activity1 : FormsAppCompatActivity
-{
-	App _app;
-
-	[Export("NavigateToTest")]
-	public bool NavigateToTest(string test)
+			Categories = new[]
+			{
+				Intent.ActionView,
+				Intent.CategoryDefault,
+				Intent.CategoryBrowsable
+			}
+			,
+			DataScheme = "http",
+			DataHost = App.AppName,
+			DataPathPrefix = "/gallery/"
+		)
+	]
+	public class Activity1 : FormsAppCompatActivity
 	{
-		return _app.NavigateToTestPage(test);
-	}
+		App _app;
 
-	public override void OnConfigurationChanged(Android.Content.Res.Configuration newConfig)
-	{
-		// we're good
-		base.OnConfigurationChanged(newConfig);
-	}
-
-	[Export("Reset")]
-	public void Reset()
-	{
-		_app.Reset();
-	}
-
-	protected override void OnCreate(Bundle bundle)
-	{
-		ToolbarResource = Resource.Layout.Toolbar;
-		TabLayoutResource = Resource.Layout.Tabbar;
-
-		base.OnCreate(bundle);
-
-		if (!Debugger.IsAttached)
-			Insights.Initialize(App.InsightsApiKey, ApplicationContext);
-
-		Forms.Init(this, bundle);
-		FormsMaps.Init(this, bundle);
-		AndroidAppLinks.Init(this);
-		Forms.ViewInitialized += (sender, e) =>
+		[Export("NavigateToTest")]
+		public bool NavigateToTest(string test)
 		{
-			//				if (!string.IsNullOrWhiteSpace(e.View.StyleId)) {
-			//					e.NativeView.ContentDescription = e.View.StyleId;
-			//				}
-		};
-		// uncomment to verify turning off title bar works. This is not intended to be dynamic really.
-		//Forms.SetTitleBarVisibility (AndroidTitleBarVisibility.Never);
-
-		var app = new App();
-		_app = app;
-
-		// When the native control gallery loads up, it'll let us know so we can add the nested native controls
-		MessagingCenter.Subscribe<NestedNativeControlGalleryPage>(this,
-			NestedNativeControlGalleryPage.ReadyForNativeControlsMessage, AddNativeControls);
-
-		// When the native binding gallery loads up, it'll let us know so we can set up the native bindings
-		MessagingCenter.Subscribe<NativeBindingGalleryPage>(this,
-			NativeBindingGalleryPage.ReadyForNativeBindingsMessage, AddNativeBindings);
-
-		LoadApplication(app);
-	}
-
-	protected override void OnDestroy()
-	{
-		base.OnDestroy();
-	}
-
-	void AddNativeBindings(NativeBindingGalleryPage page)
-	{
-		if (page.NativeControlsAdded)
-			return;
-
-		StackLayout sl = page.Layout;
-
-		var textView = new TextView(this)
-		{
-			TextSize = 14,
-			Text = "This will be text"
-		};
-
-		var viewGroup = new LinearLayout(this);
-		viewGroup.AddView(textView);
-
-		var buttonColor = new Android.Widget.Button(this) { Text = "Change label Color" };
-		buttonColor.Click += (sender, e) => textView.SetTextColor(Color.Blue.ToAndroid());
-
-		var colorPicker = new ColorPickerView(this, 200, 200);
-
-		textView.SetBinding(nameof(textView.Text), new Binding("NativeLabel"));
-		//this doesn't work because there's not TextColor property
-		//textView.SetBinding("TextColor", new Binding("NativeLabelColor", converter: new ColorConverter()));
-		colorPicker.SetBinding(nameof(colorPicker.SelectedColor),
-			new Binding("NativeLabelColor", BindingMode.TwoWay, new ColorConverter()), "ColorPicked");
-
-		sl?.Children.Add(viewGroup);
-		sl?.Children.Add(buttonColor.ToView());
-		sl?.Children.Add(colorPicker);
-
-		page.NativeControlsAdded = true;
-	}
-
-	void AddNativeControls(NestedNativeControlGalleryPage page)
-	{
-		if (page.NativeControlsAdded)
-		{
-			return;
+			return _app.NavigateToTestPage(test);
 		}
 
-		StackLayout sl = page.Layout;
-
-		// Create and add a native TextView
-		var textView = new TextView(this) { Text = "I am a native TextView", TextSize = 14 };
-		sl?.Children.Add(textView);
-
-		// Create and add a native Button 
-		var button = new Android.Widget.Button(this) { Text = "Click to change TextView font size" };
-		float originalSize = textView.TextSize;
-		button.Click += (sender, args) => { textView.TextSize = textView.TextSize == originalSize ? 24 : 14; };
-
-		sl?.Children.Add(button.ToView());
-
-		// Create a control which we know doesn't behave correctly with regard to measurement
-		var difficultControl0 = new BrokenNativeControl(this)
+		public override void OnConfigurationChanged(Droid.Content.Res.Configuration newConfig)
 		{
-			Text = "This native control doesn't play nice with sizing, which is why it's all squished to one side."
-		};
-		var difficultControl1 = new BrokenNativeControl(this)
-		{
-			Text = "Same control, but with a custom GetDesiredSize delegate to accomodate it's sizing problems."
-		};
-
-		// Add a misbehaving control 
-		sl?.Children.Add(difficultControl0);
-
-		// Add a misbehaving control with a custom delegate for GetDesiredSize
-		sl?.Children.Add(difficultControl1, SizeBrokenControl);
-
-		page.NativeControlsAdded = true;
-	}
-
-	static SizeRequest? SizeBrokenControl(NativeViewWrapperRenderer renderer,
-		int widthConstraint, int heightConstraint)
-	{
-		Android.Views.View nativeView = renderer.Control;
-
-		if (widthConstraint == 0 && heightConstraint == 0 || nativeView == null)
-		{
-			return null;
+			// we're good
+			base.OnConfigurationChanged(newConfig);z
 		}
 
-		int width = Android.Views.View.MeasureSpec.GetSize(widthConstraint);
-		int widthSpec = Android.Views.View.MeasureSpec.MakeMeasureSpec(width * 2,
-			Android.Views.View.MeasureSpec.GetMode(widthConstraint));
-		nativeView.Measure(widthSpec, heightConstraint);
-		var size = new Size(nativeView.MeasuredWidth, nativeView.MeasuredHeight);
-		return new SizeRequest(size);
-	}
-
-	public class ColorConverter : IValueConverter
-	{
-		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		[Export("Reset")]
+		public void Reset()
 		{
-			if (value is Color)
-				return ((Color)value).ToAndroid();
-
-			return null;
+			_app.Reset();
 		}
 
-		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		protected override void OnCreate(Bundle bundle)
 		{
-			if (value is Android.Graphics.Color)
-				return ((Android.Graphics.Color)value).ToColor();
+			ToolbarResource = Resource.Layout.Toolbar;
+			TabLayoutResource = Resource.Layout.Tabbar;
 
-			return null;
+			base.OnCreate(bundle);
+
+			if (!Debugger.IsAttached)
+				Insights.Initialize(App.InsightsApiKey, ApplicationContext);
+
+			Forms.Init(this, bundle);
+			FormsMaps.Init(this, bundle);
+			AndroidAppLinks.Init(this);
+			Forms.ViewInitialized += (sender, e) =>
+			{
+				//				if (!string.IsNullOrWhiteSpace(e.View.StyleId)) {
+				//					e.NativeView.ContentDescription = e.View.StyleId;
+				//				}
+			};
+			// uncomment to verify turning off title bar works. This is not intended to be dynamic really.
+			//Forms.SetTitleBarVisibility (AndroidTitleBarVisibility.Never);
+
+			var app = new App();
+			_app = app;
+
+			// When the native control gallery loads up, it'll let us know so we can add the nested native controls
+			MessagingCenter.Subscribe<NestedNativeControlGalleryPage>(this,
+				NestedNativeControlGalleryPage.ReadyForNativeControlsMessage, AddNativeControls);
+
+			// When the native binding gallery loads up, it'll let us know so we can set up the native bindings
+			MessagingCenter.Subscribe<NativeBindingGalleryPage>(this,
+				NativeBindingGalleryPage.ReadyForNativeBindingsMessage, AddNativeBindings);
+
+			LoadApplication(app);
+		}
+
+		protected override void OnDestroy()
+		{
+			base.OnDestroy();
+		}
+
+		void AddNativeBindings(NativeBindingGalleryPage page)
+		{
+			if (page.NativeControlsAdded)
+				return;
+
+			StackLayout sl = page.Layout;
+
+			var textView = new TextView(this)
+			{
+				TextSize = 14,
+				Text = "This will be text"
+			};
+
+			var viewGroup = new LinearLayout(this);
+			viewGroup.AddView(textView);
+
+			var buttonColor = new Droid.Widget.Button(this) { Text = "Change label Color" };
+			buttonColor.Click += (sender, e) => textView.SetTextColor(Color.Blue.ToAndroid());
+
+			var colorPicker = new ColorPickerView(this, 200, 200);
+
+			textView.SetBinding(nameof(textView.Text), new Binding("NativeLabel"));
+			//this doesn't work because there's not TextColor property
+			//textView.SetBinding("TextColor", new Binding("NativeLabelColor", converter: new ColorConverter()));
+			colorPicker.SetBinding(nameof(colorPicker.SelectedColor),
+				new Binding("NativeLabelColor", BindingMode.TwoWay, new ColorConverter()), "ColorPicked");
+
+			sl?.Children.Add(viewGroup);
+			sl?.Children.Add(buttonColor.ToView());
+			sl?.Children.Add(colorPicker);
+
+			page.NativeControlsAdded = true;
+		}
+
+		void AddNativeControls(NestedNativeControlGalleryPage page)
+		{
+			if (page.NativeControlsAdded)
+			{
+				return;
+			}
+
+			StackLayout sl = page.Layout;
+
+			// Create and add a native TextView
+			var textView = new TextView(this) { Text = "I am a native TextView", TextSize = 14 };
+			sl?.Children.Add(textView);
+
+			// Create and add a native Button 
+			var button = new Droid.Widget.Button(this) { Text = "Click to change TextView font size" };
+			float originalSize = textView.TextSize;
+			button.Click += (sender, args) => { textView.TextSize = textView.TextSize == originalSize ? 24 : 14; };
+
+			sl?.Children.Add(button.ToView());
+
+			// Create a control which we know doesn't behave correctly with regard to measurement
+			var difficultControl0 = new BrokenNativeControl(this)
+			{
+				Text = "This native control doesn't play nice with sizing, which is why it's all squished to one side."
+			};
+			var difficultControl1 = new BrokenNativeControl(this)
+			{
+				Text = "Same control, but with a custom GetDesiredSize delegate to accomodate it's sizing problems."
+			};
+
+			// Add a misbehaving control 
+			sl?.Children.Add(difficultControl0);
+
+			// Add a misbehaving control with a custom delegate for GetDesiredSize
+			sl?.Children.Add(difficultControl1, SizeBrokenControl);
+
+			page.NativeControlsAdded = true;
+		}
+
+		static SizeRequest? SizeBrokenControl(NativeViewWrapperRenderer renderer,
+			int widthConstraint, int heightConstraint)
+		{
+			Droid.Views.View nativeView = renderer.Control;
+
+			if (widthConstraint == 0 && heightConstraint == 0 || nativeView == null)
+			{
+				return null;
+			}
+
+			int width = Droid.Views.View.MeasureSpec.GetSize(widthConstraint);
+			int widthSpec = Droid.Views.View.MeasureSpec.MakeMeasureSpec(width * 2,
+				Droid.Views.View.MeasureSpec.GetMode(widthConstraint));
+			nativeView.Measure(widthSpec, heightConstraint);
+			var size = new Size(nativeView.MeasuredWidth, nativeView.MeasuredHeight);
+			return new SizeRequest(size);
+		}
+
+		public class ColorConverter : IValueConverter
+		{
+			public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+			{
+				if (value is Color)
+					return ((Color)value).ToAndroid();
+
+				return null;
+			}
+
+			public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+			{
+				if (value is Droid.Graphics.Color)
+					return ((Droid.Graphics.Color)value).ToColor();
+
+				return null;
+			}
 		}
 	}
-}
 #endif
 }
