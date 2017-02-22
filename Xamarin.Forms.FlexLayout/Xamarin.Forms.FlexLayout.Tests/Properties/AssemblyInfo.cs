07a0842c99e4927bbadb615539f76f3b9531a53e
Rui Marinho <me@ruimarinho.net>
2017-02-22 22:15:33 +0000

    Update to version with IVT

--- Xamarin.Forms.FlexLayout/Xamarin.Forms.FlexLayout.Tests/Properties/AssemblyInfo.cs
+++ Xamarin.Forms.FlexLayout/Xamarin.Forms.FlexLayout.Tests/Properties/AssemblyInfo.cs
@@ -2,21 +2,8 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Xamarin.Forms.FlexLayout.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Xamarin.Forms.FlexLayout.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("Xamarin.Forms.FlexLayout.UnitTests")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
--- Xamarin.Forms.FlexLayout/Xamarin.Forms.FlexLayout/Properties/AssemblyInfo.cs
+++ Xamarin.Forms.FlexLayout/Xamarin.Forms.FlexLayout/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Xamarin.Forms.FlexLayout")]
-[assembly: InternalsVisibleTo("Xamarin.Forms.Core.UnitTests")]
+[assembly: InternalsVisibleTo("Xamarin.Forms.FlexLayout.UnitTests")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
