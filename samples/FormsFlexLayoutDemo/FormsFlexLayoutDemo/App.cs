5ba94846cea71b550936a717d538142dcedbb767
Rui Marinho <me@ruimarinho.net>
2017-02-22 16:39:35 +0000

    fix

--- samples/FormsFlexLayoutDemo/FormsFlexLayoutDemo/App.cs
+++ samples/FormsFlexLayoutDemo/FormsFlexLayoutDemo/App.cs
@@ -7,21 +7,12 @@ namespace FormsFlexLayoutDemo
     {
         public App()
         {
-            //MainPage = new MasterDetailPage
-            //{
-            //    Master = new FlexOptionsPage(),
-            //    Detail = new NavigationPage(new FlexDemoPage()),
-            //    BindingContext = new FlexLayoutViewModel()
-            //};
-            var layout = new FlexLayout();
-
-            var label1 = new Label { Text = "Label 2" };
-            var label2 = new Label { Text = "Label 1" };
-
-            layout.Children.Add(label1);
-            layout.Children.Add(label2);
-
-            MainPage = new ContentPage { Content = layout };
+            MainPage = new MasterDetailPage
+            {
+                Master = new FlexOptionsPage(),
+                Detail = new NavigationPage(new FlexDemoPage()),
+                BindingContext = new FlexLayoutViewModel()
+            };
         }
 
         protected override void OnStart()
