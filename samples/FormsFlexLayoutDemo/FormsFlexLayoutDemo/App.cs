9771053b388bd8b966f255ddbede0b9de256695e
Rui Marinho <me@ruimarinho.net>
2017-02-22 16:35:34 +0000

    [Nuget] Add variable for nuspec versions

--- samples/FormsFlexLayoutDemo/FormsFlexLayoutDemo/App.cs
+++ samples/FormsFlexLayoutDemo/FormsFlexLayoutDemo/App.cs
@@ -7,12 +7,21 @@ namespace FormsFlexLayoutDemo
     {
         public App()
         {
-            MainPage = new MasterDetailPage
-            {
-                Master = new FlexOptionsPage(),
-                Detail = new NavigationPage(new FlexDemoPage()),
-                BindingContext = new FlexLayoutViewModel()
-            };
+            //MainPage = new MasterDetailPage
+            //{
+            //    Master = new FlexOptionsPage(),
+            //    Detail = new NavigationPage(new FlexDemoPage()),
+            //    BindingContext = new FlexLayoutViewModel()
+            //};
+            var layout = new FlexLayout();
+
+            var label1 = new Label { Text = "Label 2" };
+            var label2 = new Label { Text = "Label 1" };
+
+            layout.Children.Add(label1);
+            layout.Children.Add(label2);
+
+            MainPage = new ContentPage { Content = layout };
         }
 
         protected override void OnStart()
