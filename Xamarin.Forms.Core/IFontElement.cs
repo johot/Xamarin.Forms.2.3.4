f2353761efe62fdaf96fe119c1a1074e870bd8fb
Stephane Delcroix <stephane@delcroix.org>
2017-02-23 12:46:29 +0100

    [Core] Share BP accross IFontElement implementors

--- Xamarin.Forms.Core/IFontElement.cs
+++ Xamarin.Forms.Core/IFontElement.cs
@@ -1,11 +1,16 @@
 namespace Xamarin.Forms
 {
-	internal interface IFontElement
+	interface IFontElement
 	{
+		//note to implementor: implement the properties publicly
 		FontAttributes FontAttributes { get; }
-
 		string FontFamily { get; }
-
 		double FontSize { get; }
+
+		//note to implementor: but implement the methods explicitly
+		void OnFontFamilyChanged(string oldValue, string newValue);
+		void OnFontSizeChanged(double oldValue, double newValue);
+		double FontSizeDefaultValueCreator();
+		void OnFontAttributesChanged(FontAttributes oldValue, FontAttributes newValue);
 	}
 }
